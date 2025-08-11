659039e97eeb13231dbaf6f819c96f6ff0c7bd14
Paul Galjan <galjan@gmail.com>
2025-08-11 06:22:39 -0400

    rest api migration

--- power-query/customers.m
+++ power-query/customers.m
@@ -1,3 +1,4 @@
+/* Old Sharepoint.Tables version
 let
     siteUrl = Excel.CurrentWorkbook(){[Name="siteUrl"]}[Content]{0}[Column1],
     listName = Excel.CurrentWorkbook(){[Name="custList"]}[Content]{0}[Column1],
@@ -8,3 +9,31 @@ let
     #"Expanded Website" = Table.ExpandRecordColumn(#"Expanded Items", "Website", {"Url"}, {"Url"})
 in
     #"Expanded Website"
+*/
+
+// Rest API
+let
+    siteUrl = Excel.CurrentWorkbook(){[Name="siteUrl"]}[Content]{0}[Column1],
+    listName = Excel.CurrentWorkbook(){[Name="custList"]}[Content]{0}[Column1],
+    
+    // Include CustomerName field
+    restUrl = siteUrl & "_api/web/lists/getbytitle('" & listName & "')/items?$select=ID,Title,CustomerName,PrimaryContact,PrimaryContactTitle,AlternateContact,AlternateContactTitle,AlternateContact2,AlternateContact2Title,Website,Modified,Created",
+    
+    Source = Json.Document(Web.Contents(restUrl, [
+        Headers=[
+            #"Accept" = "application/json;odata=verbose"
+        ]
+    ])),
+    
+    Results = Source[d][results],
+    
+    #"Converted to Table" = Table.FromList(Results, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+    
+    #"Expanded Column1" = Table.ExpandRecordColumn(#"Converted to Table", "Column1", 
+        {"ID", "Title", "CustomerName", "PrimaryContact", "PrimaryContactTitle", "AlternateContact", "AlternateContactTitle", "AlternateContact2", "AlternateContact2Title", "Website", "Modified", "Created"}, 
+        {"Id", "Title", "Customer Name", "PrimaryContact", "PrimaryContactTitle", "AlternateContact", "AlternateContactTitle", "AlternateContact2", "AlternateContact2Title", "Website", "Modified", "Created"}),
+    
+    #"Expanded Website" = Table.ExpandRecordColumn(#"Expanded Column1", "Website", {"Url"}, {"Url"}),
+    #"Removed Columns" = Table.RemoveColumns(#"Expanded Website",{"Title"})
+in
+    #"Removed Columns"
--- power-query/opportunities.m
+++ power-query/opportunities.m
@@ -1,3 +1,4 @@
+/* Old Sharepoint.Tables version
 let
     siteUrl = Excel.CurrentWorkbook(){[Name="siteUrl"]}[Content]{0}[Column1],
     listName = Excel.CurrentWorkbook(){[Name="oppList"]}[Content]{0}[Column1],
@@ -7,3 +8,35 @@ let
     #"Expanded Items" = Table.ExpandTableColumn(#"Removed Columns", "Items", {"Id", "Title", "Status", "Stage", "Amount", "Probability", "CustomerId", "Close", "NextMilestone", "NextMilestoneDate", "Comment Log", "Customer"}, {"Id", "Title", "Status", "Stage", "Amount", "Probability", "CustomerId", "Close", "NextMilestone", "NextMilestoneDate", "Comment Log", "Customer"})
 in
     #"Expanded Items"
+
+*/
+
+// Rest API Version
+
+let
+    siteUrl = Excel.CurrentWorkbook(){[Name="siteUrl"]}[Content]{0}[Column1],
+    listName = Excel.CurrentWorkbook(){[Name="oppList"]}[Content]{0}[Column1],
+    
+    // Use CustomerIdId to get the lookup ID value
+    restUrl = siteUrl & "_api/web/lists/getbytitle('" & listName & "')/items?$select=ID,Title,OpportunityName,Status,OpportunityStage,Amount,Probability,Close,NextMilestoneDate,NextMilestone,CustomerIdId,CommentLog,OpportunityOwner",
+    
+    Source = Json.Document(Web.Contents(restUrl, [
+        Headers=[
+            #"Accept" = "application/json;odata=verbose"
+        ]
+    ])),
+    
+    Results = Source[d][results],
+    
+    #"Converted to Table" = Table.FromList(Results, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+    
+    #"Expanded Column1" = Table.ExpandRecordColumn(#"Converted to Table", "Column1", 
+        {"ID", "Title", "OpportunityName", "Status", "OpportunityStage", "Amount", "Probability", "Close", "NextMilestoneDate", "NextMilestone", "CustomerIdId", "CommentLog", "OpportunityOwner"}, 
+        {"ID", "Title", "OpportunityName", "Status", "Stage", "Amount", "Probability", "Close", "NextMilestoneDate", "NextMilestone", "CustomerId", "Comment Log", "OpportunityOwner"}),
+    #"Extracted Text Before Delimiter" = Table.TransformColumns(#"Expanded Column1", {{"Close", each Text.BeforeDelimiter(_, "T"), type text}}),
+    #"Changed Type" = Table.TransformColumnTypes(#"Extracted Text Before Delimiter",{{"Close", type date}}),
+    #"Extracted Text Before Delimiter1" = Table.TransformColumns(#"Changed Type", {{"NextMilestoneDate", each Text.BeforeDelimiter(_, "T"), type text}}),
+    #"Changed Type1" = Table.TransformColumnTypes(#"Extracted Text Before Delimiter1",{{"NextMilestoneDate", type date}}),
+    #"Removed Columns" = Table.RemoveColumns(#"Changed Type1",{"Title"})
+in
+    #"Removed Columns"
